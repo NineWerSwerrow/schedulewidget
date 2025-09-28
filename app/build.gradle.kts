87cd7644ca46d9f523abc387cf28564af674d586
ninewer <ninewerswerw@gmail.com>
2025-09-28 09:15:40 +0700

    card height decrease

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 2
-        versionName = "2.3"
+        versionName = "2.4"
 
         vectorDrawables {
             useSupportLibrary = true
